ddd2e522dcdfcc6522111a70ea73e621255acca4
Anton Gustafsson <ajacobsson98@gmail.com>
2021-12-16 22:22:35 +0100

    fix markers

--- src/MapChart.js
+++ src/MapChart.js
@@ -108,7 +108,6 @@ class MapChart extends React.Component {
 
           </div>
         </div>
-
       );
     }
 
--- src/markers.js
+++ src/markers.js
@@ -22,43 +22,43 @@ export function getMarkers(activeIds) {
 const markers = [
     {
         id: 1,
-        name: "Himlabaden",
-        coordinates: [17.289075083375486, 62.394040657166876]
+        name: "Stenungsund Arena",
+        coordinates: [11.826849756213939, 58.05582129989261]
     },
     {
         id: 2,
-        name: "Lost City på Gustavik",
-        coordinates: [15.194292311990083, 59.260323160175844]
-    },
-    {
-        id: 3,
-        name: "Maserhallen & aqua nova",
-        coordinates: [15.411557827296596, 60.49202792492629]
-    },
-    {
-        id: 4,
-        name: "Medley Vilundabadet",
-        coordinates: [17.919339027970643, 59.51731494690123]
-    },
-    {
-        id: 5,
-        name: "Eriksdalsbadet",
-        coordinates: [18.075619429186784, 59.30494259556112]
-    },
-    {
-        id: 6,
         name: "Vattenpalatset Vänerparken",
         coordinates: [12.317164457671774, 58.375867389201886]
     },
     {
-        id: 7,
+        id: 3,
+        name: "Actic Lerum - Vattenpalatset",
+        coordinates: [12.268923013777709, 57.77264527377351]
+    },
+    {
+        id: 4,
+        name: "Rosenlundsbadet",
+        coordinates: [14.228334191029864, 57.7863524055531]
+    },
+    {
+        id: 5,
         name: "Halmstad Arena Äventyrsbad",
         coordinates: [12.888400041884172, 56.6746067972053]
     },
     {
+        id: 6,
+        name: "Lost City på Gustavik",
+        coordinates: [15.194292311990083, 59.260323160175844]
+    },
+    {
+        id: 7,
+        name: "Kokpunkten Actionbad",
+        coordinates: [16.566521283260982, 59.60722044167188]
+    },
+    {
         id: 8,
-        name: "Rosenlundsbadet",
-        coordinates: [14.228334191029864, 57.7863524055531]
+        name: "Medley Vilundabadet",
+        coordinates: [17.919339027970643, 59.51731494690123]
     },
     {
         id: 9,
@@ -67,7 +67,7 @@ const markers = [
     },
     {
         id: 10,
-        name: "Actic Lerum - Vattenpalatset",
-        coordinates: [12.268923013777709, 57.77264527377351]
-    }
+        name: "Maserhallen & aqua nova",
+        coordinates: [15.411557827296596, 60.49202792492629]
+    },
 ];
